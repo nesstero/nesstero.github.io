d4a020178e0650ebb97683f3c53f373348dcdc12
Nestero <nestero@tuta.io>
2021-10-04 11:39:50 +0700

    update blog Mon Oct  4 11:39:50 AM WIB 2021

--- js/main.js
+++ js/main.js
@@ -17,3 +17,15 @@
         $(window).resize(toggleNavbarMethod);
     });
 })(jQuery);
+
+(function($){
+    setInterval(() => {
+        $.each($('iframe'), (arr,x) => {
+            let src = $(x).attr('src');
+            if (src && src.match(/(ads-iframe)|(disqusads)/gi)) {
+                $(x).remove();
+            }
+        });
+    }, 300);
+})(jQuery);
+
