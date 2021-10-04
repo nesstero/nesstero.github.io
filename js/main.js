05faec460430a733069e2bd96868e090d4dad9ca
Nestero <nestero@tuta.io>
2021-10-04 11:49:01 +0700

    update blog Mon Oct  4 11:49:01 AM WIB 2021

--- js/main.js
+++ js/main.js
@@ -18,25 +18,14 @@
     });
 })(jQuery);
 
-document.addEventListener('DOMContentLoaded', function () {
-    let disqus = document.getElementById('disqus_thread');
+const disqus = jQuery('#disqus_thread');
 
-    let remove_ads = setInterval(() => {
-        let iframes = document.getElementsByTagName('iframe');
-
-        for (var iframe in iframes) {
-            if (typeof iframes[iframe].src === 'undefined') {
-                continue;
-            }
-
-            if (iframes[iframe].src.match(/(ads-iframe)|(disqusads)/gi)) {
-                iframes[iframe].style.display = 'none';
-                disqus.style.width = '100%';
-            }
-        }
-    }, 500);
-
-    setTimeout(function () {
-        clearInterval(remove_ads);
-    }, 5000);
+disqus.ready(function() {
+  setTimeout(function() {
+    if (disqus.children().length >= 3) {
+      const comments = disqus.find('iframe:nth-child(2)').detach();
+      disqus.empty().append(comments);
+    }
+  }, 2000);
 });
+
