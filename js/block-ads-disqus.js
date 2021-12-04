21e73cf9d0fe2ec11cb0aa792c84f469baa88a64
Nestero <nestero@tuta.io>
2021-12-05 01:24:46 +0700

    update blog Sun Dec  5 01:24:46 WIB 2021

--- js/block-ads-disqus.js
+++ js/block-ads-disqus.js
@@ -1,9 +1,1 @@
-const disqus = jQuery('#disqus_thread');
-disqus.ready(function() {
-  setTimeout(function() {
-    if (disqus.children().length >= 3) {
-      const comments = disqus.find('iframe:nth-child(2)').detach();
-      disqus.empty().append(comments);
-    }
-  }, 2000);
-});
+const disqus=jQuery("#disqus_thread");disqus.ready(function(){setTimeout(function(){if(disqus.children().length>=3){const a=disqus.find("iframe:nth-child(2)").detach();disqus.empty().append(a)}},2e3)});
